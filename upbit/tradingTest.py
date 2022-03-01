ab7e1a82bb323ba39ab3082bd66ef88b536ce938
sim <dhksdn4689@naver.com>
2022-03-01 17:11:30 +0900

    메인 나스닥,비트코인 1일 차트 추가

--- upbit/tradingTest.py
+++ upbit/tradingTest.py
@@ -49,30 +49,31 @@ def get_current_price(ticker):
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-print(get_balance("KRW"))
 
-# while True:
-#     try:
-#         now = datetime.datetime.now()
-#         start_time = get_start_time("KRW-BTC")
-#         end_time = start_time + datetime.timedelta(days=1)
-#
-#         if start_time < now < end_time - datetime.timedelta(seconds=10):
-#             target_price = get_target_price("KRW-BTC", 0.5)
-#             ma15 = get_ma15("KRW-BTC")
-#             current_price = get_current_price("KRW-BTC")
-#             if target_price < current_price and ma15 < current_price:
-#                 krw = get_balance("KRW")
-#                 if krw > 5000:
-#                     buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
-#
-#         else:
-#             btc = get_balance("BTC")
-#             if btc > 0.00008:
-#                 sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
-#
-#         time.sleep(1)
-#     except Exception as e:
-#         print(e)
-#
-#         time.sleep(1)
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1)
+
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-BTC", 0.5)
+            ma15 = get_ma15("KRW-BTC")
+            current_price = get_current_price("KRW-BTC")
+            if target_price < current_price and ma15 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    print("1")
+
+        else:
+            btc = get_balance("BTC")
+            if btc > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                print("2")
+
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        print("3")
+        time.sleep(1)
