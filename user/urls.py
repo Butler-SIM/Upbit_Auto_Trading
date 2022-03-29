761ebad84b82e78ba267c33fc84cea1bae32fbaa
simwanwoo <dhksdn4689@naver.com>
2022-03-29 16:09:15 +0900

    로그아웃 추가 푸터 수정

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,7 @@ app_name = 'user'
 urlpatterns = [
     path('', user, name='user'),
     path('login', LoginView.as_view(), name='login'),
+    path('logout', logout, name='logout'),
     path('join', JoinView.as_view(), name='join'),
     path('kakaoLogin', KakaoSignInView.as_view(), name='KakaoSignInView'),
     path('accounts/signin/kakao/callback', KaKaoSignInCallBackView.as_view(), name='KaKaoSignInCallBackView'),
