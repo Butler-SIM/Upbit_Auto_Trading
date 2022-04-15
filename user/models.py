2c44969ae32cc29b8170fdf337682c2e7b0ff078
simwanwoo <dhksdn4689@naver.com>
2022-04-15 16:56:40 +0900

    2022-04-13 유저모델 오타ㅅ 수정

--- user/models.py
+++ user/models.py
@@ -10,7 +10,7 @@ class UserModel(models.Model):
     upbit_secret_key = models.TextField(default='', verbose_name="업비트 시크릿 키")
     email = models.EmailField(null=True, blank=True, verbose_name='이메일')
     # 유저 상태 - 1 : 정상, 0 : 정지, 2~9 : 등급, 77 : 탈퇴
-    user_staus = models.CharField(max_length=3, default='1', verbose_name='유저 상태')
+    user_status = models.CharField(max_length=3, default='1', verbose_name='유저 상태')
     auto_trading_status = models.CharField(max_length=3, default='0', verbose_name='자동 매매 활성화 상태')
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name="생성 날짜")
 
